5d06575b8e35905a35b691204be2001e6829c312
PyNvimDev <86599386+pynvimdev@users.noreply.github.com>
2021-10-30 15:19:14 +0530

    Fixed bug and used art

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
+# PyGit
+# Author : Hari
 import os 
-print('Welcome to pygit a git push and pull manager (Case Sensitive Inputs)')
-token = input('Enter github token: ')
-username = input('Enter Username: ')
-repo = input('Enter Repo Name: ')
-command = str('git remote set-url origin https://' + token + '@github.com'+ username + repo + '.git')
-push = str('git push origin HEAD:master')
+from os import name, system
+from art import *
 
-os.system(command)
-os.system(push)
- # TODO
-command2 = 'git remote add origin git@github.com:' + username + '/' + repo + '.git'
-os.system(command2)
+def clear():
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 
-git remote add origin git@github.com:pynvimdev/pygit.git
-os.system('git branch -M main')
-os.system('git push -u origin main')
+def main():
+    clear()
+    tprint('PyGit')
+    print('Welcome to pygit, A git push and pull manager for github(Case Sensitive Inputs)')
+    branch = input('Enter the branch to upload: ')
+    username = input('Enter Username: ')
+    repo = input('Enter Repo Name: ')
+    token = input('Enter github token: ')
+    command = str('git remote set-url origin https://' + token + '@github.com' + '/'+ username + '/' + repo + '.git')
+    command2 = 'git remote add origin git@github.com:' + username + '/' + repo + '.git'
+    push = str(f'git push origin HEAD:{branch}')
+    os.system(command)
+    os.system(push)
+    os.system(command2)
+
+if __name__ == "__main__":
+    main()
