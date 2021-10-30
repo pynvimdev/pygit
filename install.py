40956b7a416f742f6b542d8c638fca0797c9f4b9
PyNvimDev <86599386+pynvimdev@users.noreply.github.com>
2021-10-30 15:21:27 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,7 @@
+from os import name, system
+print('Installing depenedencies')
 if name == 'nt':
      _ = system('pip install art ')
 else:
      _ = system('pip3 install art')
+print('Done! Your good to run')
